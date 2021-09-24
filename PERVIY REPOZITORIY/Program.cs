fedd9c31d87dd3a5e640bf9a6f065a6d206ace7f
Afren12am <77343003+Afren12am@users.noreply.github.com>
2021-09-24 19:54:56 +0300

    Add files via upload

---
+++ PERVIY REPOZITORIY/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PERVIY_REPOZITORIY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я ПЕРВЫЙ РЕПОЗИТОРИЙ");
+        }
+    }
+}
