10deab7867d4ade434167125642ecae995434cae
Afren12am <77343003+Afren12am@users.noreply.github.com>
2021-09-24 20:21:03 +0300

    Add files via upload

---
+++ PERVIY REPOZITORIY/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int Width;
+            int Height;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Width = Console.WindowWidth;
+            Height = Console.WindowHeight;
+            string text = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ/-+=/][.,";
+            while (true)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.SetCursorPosition(rand.Next(0, Width), rand.Next(0, Height));
+                    Console.WriteLine(text[rand.Next(0, 45)]);
+
+                }
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.SetCursorPosition(rand.Next(0, Width), rand.Next(0, Height));
+                    Console.Write(" ");
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
